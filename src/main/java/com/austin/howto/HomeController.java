d4863540ed6751d47f036edb95aa12528c5a81a8
GBTC440019UR <GBTC440019UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-12 15:13:28 -0500

    first commit

---
+++ src/main/java/com/austin/howto/HomeController.java
@@ -0,0 +1,29 @@
+package com.austin.howto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/tutorial")
+    public String getStarted() {
+        return "tutorial";
+    }
+
+    @RequestMapping("/faq")
+    public String faq() {
+        return "faq";
+    }
+
+    @RequestMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+
+}
